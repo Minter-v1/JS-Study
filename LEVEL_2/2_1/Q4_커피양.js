c22e7f7f22d7013166ba096b17185197ea1c72ea
Minter0817 <adf5858@naver.com>
2025-03-17 04:27:08 +0900

    Update: 실습 코드 3.17.Mon

---
+++ LEVEL_2/2_1/Q4_커피양.js
@@ -0,0 +1,9 @@
+function myCoffee(coffee) {
+    var ans = coffee
+    var ans2 = ans * 2 / 3
+    var ans3 = ans2 * 2 / 3
+
+    console.log(ans + ans2 + ans3);
+}
+
+myCoffee(360);
---
+++ LEVEL_2/2_2/test.js
@@ -0,0 +1,8 @@
+setTimeout(function () {
+    console.log("안녕");
+}, 1000); // 시간은 ms단위 
+
+
+setInterval(function () {
+    console.log("바보");
+}, 1000)
